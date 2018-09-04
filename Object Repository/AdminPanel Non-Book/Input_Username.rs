f20b5557c26b6de8c29b48669b80eecd6e215a38
danandio <danandio@172.16.141.199>
2018-09-04 18:01:43 +0700

    enter new test case n Object for adminpanel

---
+++ Object Repository/AdminPanel Non-Book/Input_Username.rs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_Username</name>
+   <tag></tag>
+   <elementGuidId>45a434df-8aaa-4bf7-b649-c4e4e1041e82</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;inputEmail&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
